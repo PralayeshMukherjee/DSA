7e9acb9a52d9c6884f343ed8be31742619485424
pralayesh <rajmukherjee2807@gmail.com>
2023-08-16 11:09:09 +0530

    remove the useless lines in the code

---
+++ insertioninarray.java
@@ -0,0 +1,28 @@
+public class insertioninarray{
+
+    
+    int arr[] = null;
+    
+    public insertioninarray(int i) {
+        arr = new int[i];
+        for(int j=0;j<arr.length;j++) {
+            arr[j] = Integer.MIN_VALUE;
+        }
+	}
+	
+    
+    public void insert(int location, int valueofInsert) {
+        try {
+            if(arr[location] == Integer.MIN_VALUE) {
+                arr[location] = valueofInsert;
+                System.out.println("successfully inserted");
+            } else {
+                System.out.println("this cell is already occupied");
+            }
+        }catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index to access array!");
+        }
+    }
+    // runtime complexity is Big O(1).
+    
+}
--- main.java
+++ main.java
@@ -1,16 +1,9 @@
 class main {
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
         insertioninarray sda = new insertioninarray(10);
         sda.insert(0,0);
         sda.insert(1,10);
         sda.insert(2,30);
         sda.insert(12,120);
-    }*/
-    public static void main(String[] args) {
-        insertioninarray arr = new insertioninarray(5);
-        arr.insert(0,0);
-        arr.insert(1,10);
-        arr.insert(2,30);
-        arr.insert(12,120);
     }
 }
