ec5fe4289339941974230becd5d548e78fb9f370
pralayesh <rajmukherjee2807@gmail.com>
2023-08-16 15:20:04 +0530

    add the code for search the array

--- main1.java
+++ main1.java
@@ -1,10 +1,11 @@
 public class main1 {
     public static void main(String[] args) {
-        arraytraversal arr = new arraytraversal(10);
+        insertioninarray arr = new insertioninarray(10);
         arr.insert(0, 10);
         arr.insert(1, 20);
         arr.insert(2, 30);
+        arr.insert(10, 5);
 
-        arr.traverseArray();
+        arr.searchArray(10);
     }
 }
