2beffcf03533b8eafac293494312d8eee7cf4111
pralayesh <rajmukherjee2807@gmail.com>
2023-08-16 15:10:40 +0530

    change the file name

---
+++ main2.java
@@ -0,0 +1,10 @@
+public class main2 {
+    public static void main(String[] args) {
+        arraytraversal arr = new arraytraversal(10);
+        arr.insert(0, 10);
+        arr.insert(1, 20);
+        arr.insert(2, 30);
+
+        arr.traverseArray();
+    }
+}
