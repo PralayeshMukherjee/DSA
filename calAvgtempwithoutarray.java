e7115d53c5804a9f7d2ccc7eb6f775fea6d1504b
pralayesh <rajmukherjee2807@gmail.com>
2023-08-19 22:21:02 +0530

    show that how you can calculate the average temprature without creating an array but in the next file I'm going to add a file where I'm explain how you do this with an array because here I'm using scanner class if anyone to add the number of days or edit the temprature than he/she has to be run again the same code

--- calAvgtempwithoutarray.java
+++ calAvgtempwithoutarray.java
@@ -1,10 +1,27 @@
-/*
- Qs) Find Number of Days Above Average Temprature 
-
- Input statement 1 :- How many day's temprature? - 2
- Input statement 2 :- Day 1's high temp: 10
- Input statement 3 :- Day 2's high temp: 20
-
- Output :- Average = 15.0
-           2 day's above average 
-*/
+import java.util.Scanner;
+public class calAvgtempwithoutarray{
+    public static void main(String[] args) {
+        //  Qs) Find Number of Days Above Average Temprature 
+        
+        //  Input statement 1 :- How many day's temprature? - 2
+        //  Input statement 2 :- Day 1's high temp: 10
+        //  Input statement 3 :- Day 2's high temp: 20
+        
+        //  Output :- Average = 15.0
+        //    2 day's above average 
+        
+        
+        // Write the answer without adding an array
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many day's tempratures? =>");
+        int numberOfDays = sc.nextInt();
+        int sum = 0;
+        for(int i=1;i<=numberOfDays;i++) {
+            System.out.println("Day "+i+"'s high temp: ");
+            int next = sc.nextInt();
+            sum += next;
+        }
+        double average = sum / numberOfDays;
+        System.out.println("Average Temprature = "+average);
+    }
+}
