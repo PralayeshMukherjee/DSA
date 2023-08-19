3010bce1934c9af9cff15459514f09e0eb62f9cf
pralayesh <rajmukherjee2807@gmail.com>
2023-08-19 23:17:17 +0530

    write the Question of Calculate Average Temprature and also define Input and Output content

---
+++ CalAvgTempUsingArray.java
@@ -0,0 +1,15 @@
+public class CalAvgTempUsingArray{
+    public static void main(String[] args) {
+        //  Qs) Find Number of Days Above Average Temprature 
+        
+        //  Input statement 1 :- How many day's temprature? - 2
+        //  Input statement 2 :- Day 1's high temp: 10
+        //  Input statement 3 :- Day 2's high temp: 20
+        
+        //  Output :- Average = 15.0
+        //    1 day's above average 
+        
+        
+        // Write the answer without adding an array
+    }
+}
