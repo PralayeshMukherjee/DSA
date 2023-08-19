5d81ccd05547c3f029d1c0507d2a5f03b0ce6e5a
pralayesh <rajmukherjee2807@gmail.com>
2023-08-19 16:31:26 +0530

    write the Question and input output statements

---
+++ calAvgtempwithoutarray.java
@@ -0,0 +1,10 @@
+/*
+ Qs) Find Number of Days Above Average Temprature 
+
+ Input statement 1 :- How many day's temprature? - 2
+ Input statement 2 :- Day 1's high temp: 10
+ Input statement 3 :- Day 2's high temp: 20
+
+ Output :- Average = 15.0
+           2 day's above average 
+*/
