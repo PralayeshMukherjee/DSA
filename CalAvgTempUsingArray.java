d191971b36a4e916b40cb6031f331d98ef1d81c6
pralayesh <rajmukherjee2807@gmail.com>
2023-08-19 23:38:38 +0530

    add a whole example that shows how can you implement a java code which is calculate the average temperature of some day's that you input and how many days temperature is above the average temperature using an array data structure

--- CalAvgTempUsingArray.java
+++ CalAvgTempUsingArray.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CalAvgTempUsingArray{
     public static void main(String[] args) {
         //  Qs) Find Number of Days Above Average Temprature 
@@ -11,5 +13,29 @@ public class CalAvgTempUsingArray{
         
         
         // Write the answer without adding an array
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many day's tempratures? ");
+        int numberOfDays = sc.nextInt();
+        int temps[] = new int[numberOfDays];
+        // record temperature and find average
+        int sum = 0;
+        for(int i=0;i<numberOfDays;i++) {
+            System.out.println("Day "+(i+1)+"'s high temp: ");
+            temps[i] = sc.nextInt();
+            sum += temps[i];
+        }
+
+        double average = sum / numberOfDays;
+        System.out.println("Average Temperature: "+average);
+
+        // count days above average 
+        int above = 0;
+        for(int i=0;i<temps.length;i++) {
+            if(temps[i]>average) {
+                above++;
+                System.out.println(above+" day's above average!");
+            }
+        }
     }
 }
