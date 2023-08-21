637a943bb91cc398bf5dd3a847dbb35d68b1839e
pralayesh <rajmukherjee2807@gmail.com>
2023-08-21 23:08:10 +0530

    write the question and also input & output statements

---
+++ CalAvgTempsUsingArray.java
@@ -0,0 +1,11 @@
+public class CalAvgTempsUsingArray{
+    public static void main(String[] args) {
+        // Find Number of Days Above Average Temperature
+        // Input Statement 1 :- How many day's temperature? =>
+        // Input Statement 2 :- Day 1's high temperature: 10
+        // Input Statement 3 :- Day 2's high temperature: 20
+
+        // Output Statement 1 :- Average = 15.0
+        // Output Statement 2 :- 1 day's above average.
+    }
+}
