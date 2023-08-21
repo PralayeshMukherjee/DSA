a28e2c8ecabd5a64fd8ba7155072fb603fb81fc4
pralayesh <rajmukherjee2807@gmail.com>
2023-08-21 23:45:19 +0530

    add the code and explain the it that how you can Calculate the Average Temperature of some days and also calculate how many days are above the average temperature

--- CalAvgTempsUsingArray.java
+++ CalAvgTempsUsingArray.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CalAvgTempsUsingArray{
     public static void main(String[] args) {
         // Find Number of Days Above Average Temperature
@@ -7,5 +9,29 @@ public class CalAvgTempsUsingArray{
 
         // Output Statement 1 :- Average = 15.0
         // Output Statement 2 :- 1 day's above average.
+        
+        // Ans:
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many day's temperature? =>");
+        int numberOfDays = sc.nextInt();
+        int [] temperature = new int[numberOfDays];
+        int sum = 0;
+        for(int i=0;i<numberOfDays;i++) {
+            System.out.println("Day "+(i+1)+"'s high temperature: ");
+            temperature[i] = sc.nextInt();
+            sum += temperature[i];
+        }
+        
+        double average = sum / numberOfDays;
+        System.out.println("Average Temperature: "+average);
+
+        int above = 0;
+        for(int i=0;i<temperature.length;i++) {
+            if(temperature[i] > average) {
+                above++;
+                System.out.println(above+" day's above average!");
+            }
+        }
+        sc.close();
     }
 }
