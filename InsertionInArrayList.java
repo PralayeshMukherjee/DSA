e2832fc13d803a9df39eb0c9e7c4278343c570b1
pralayesh <rajmukherjee2807@gmail.com>
2023-08-26 23:29:22 +0530

    use another add method to insert an element at the specific index and the last element shift to right box and I'm use this for the Integer ArrayList

--- InsertionInArrayList.java
+++ InsertionInArrayList.java
@@ -7,6 +7,7 @@ public class InsertionInArrayList{
         numbers.add(1);
         numbers.add(2);
         numbers.add(3);
+        numbers.add(2,4); // if I use this method to inserting an element then the specific element implement at the mention index of an ArrayList and the past element which was stand at the mention index, move to the right element box of an ArrayList.
         System.out.println(numbers);
         // ArrayList for a String
         ArrayList<String> names = new ArrayList<String>();
