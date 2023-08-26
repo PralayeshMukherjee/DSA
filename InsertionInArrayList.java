3ef93e6a997e25fff1ed8f758acbc71b8aabdeb4
pralayesh <rajmukherjee2807@gmail.com>
2023-08-26 23:08:33 +0530

    write how you can use add method to insert some elements in an ArrayList

---
+++ InsertionInArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class InsertionInArrayList{
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        System.out.println(numbers);
+    }
+}
