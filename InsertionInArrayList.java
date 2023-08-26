595854324575cfcd72d40c3a1ebcd4d277531e4d
pralayesh <rajmukherjee2807@gmail.com>
2023-08-26 23:42:44 +0530

    add the time and space complexity for the second add method

--- InsertionInArrayList.java
+++ InsertionInArrayList.java
@@ -8,6 +8,7 @@ public class InsertionInArrayList{
         numbers.add(2);
         numbers.add(3); // time & space complexity is Big O(1)
         numbers.add(2,4); // if I use this method to inserting an element then the specific element implement at the mention index of an ArrayList and the past element which was stand at the mention index, move to the right element box of an ArrayList.
+        // time complexity for this add method is Big O(n) but the space complexity is Big O(1).
         System.out.println(numbers);
         // ArrayList for a String
         ArrayList<String> names = new ArrayList<String>();
