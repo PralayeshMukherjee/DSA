5f9b4fd6a4c66706bbbf7f67dd306d8575341ede
pralayesh <rajmukherjee2807@gmail.com>
2023-08-27 21:25:42 +0530

    none

---
+++ TraversalOfArrayList.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class TraversalOfArrayList{
+    public static void main(String[] args) {
+        // Create an ArrayList which type is names
+        ArrayList<String> names = new ArrayList<String>();
+    }
+}
