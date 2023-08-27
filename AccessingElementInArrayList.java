db63b111378380775b6760ce193ded8d3f7e7d2f
pralayesh <rajmukherjee2807@gmail.com>
2023-08-27 20:41:54 +0530

    write the get method to access the element in an ArrayList and print this by using 2 Strings but you can directly print this

--- AccessingElementInArrayList.java
+++ AccessingElementInArrayList.java
@@ -2,11 +2,19 @@ import java.util.ArrayList;
 
 public class AccessingElementInArrayList{
     public static void main(String[] args) {
+        // create an ArrayList
         ArrayList<String> names = new ArrayList<String>();
+        // insert all names
         names.add("raj");
         names.add("sarbojit");
         names.add("maharnab");
         names.add(2,"anjan");
         System.out.println(names);
+        // Accessing Elements in ArrayList
+        String nameAt3rdIndex = names.get(2);
+        String nameAt4thIndex = names.get(3);
+        System.out.println(nameAt3rdIndex);
+        System.out.println(nameAt4thIndex);
+
     }
 }
