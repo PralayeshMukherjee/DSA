85aad11f1c6b769869f35e7e58b483beb9bdce77
pralayesh <rajmukherjee2807@gmail.com>
2023-08-27 22:30:02 +0530

    use add method to inserting an element in an ArrayList

--- TraversalOfArrayList.java
+++ TraversalOfArrayList.java
@@ -4,5 +4,11 @@ public class TraversalOfArrayList{
     public static void main(String[] args) {
         // Create an ArrayList which type is String
         ArrayList<String> names = new ArrayList<String>();
+        // Inserting an element in an ArrayList
+        names.add("raj");
+        names.add("maharnab");
+        names.add("sarbojit");
+        names.add(2,"anjan");
+        System.out.println(names);
     }
 }
