1dc573598edf1b85b13730f315f33513d123aa04
pralayesh <rajmukherjee2807@gmail.com>
2023-08-27 21:26:16 +0530

    Create an ArrayList which type is String

--- TraversalOfArrayList.java
+++ TraversalOfArrayList.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class TraversalOfArrayList{
     public static void main(String[] args) {
-        // Create an ArrayList which type is names
+        // Create an ArrayList which type is String
         ArrayList<String> names = new ArrayList<String>();
     }
 }
