4cf481c9c9bb746638e3aeda0472c68d395bf9b4
pralayesh <rajmukherjee2807@gmail.com>
2023-08-29 12:49:29 +0530

    write the ArrayList which type is String and add all elements

---
+++ DeleteAnElementForArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class DeleteAnElementForArrayList{
+    public static void main(String[] args) {
+        // Creating an ArrayList 
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("raj");
+        names.add("sarbojit");
+        names.add("maharnob");
+        names.add("anjan");
+        names.add("arpan");
+        System.out.println(names);
+    }
+}
