ac1eb6bc2dbb8788b7c40238516b4d721bd48f41
pralayesh <rajmukherjee2807@gmail.com>
2023-08-31 15:03:27 +0530

    add the question of interview

---
+++ interviewQuestion1.java
@@ -0,0 +1,7 @@
+public class interviewQuestion1{
+    public static void main(String[] args) {
+        // Create a function which calculates the sum and product of elements of Array
+        // Find the complexity for created method
+        
+    }
+}
