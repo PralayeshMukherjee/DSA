45bd6e45e1f5426e161504309e5be2779cc46549
pralayesh <rajmukherjee2807@gmail.com>
2023-08-31 22:20:42 +0530

    complete the first Question where I'm using a function and use 2 for loops. In the first loop used for sum of all element in an array and the second loop used for product of all element in an array and print them. I'm also create a main function and also create new object to access the new function. I'm creating an array in the main function & call the new function

--- interviewQuestion1.java
+++ interviewQuestion1.java
@@ -1,7 +1,24 @@
 public class interviewQuestion1{
     public static void main(String[] args) {
-        // Create a function which calculates the sum and product of elements of Array
-        // Find the complexity for created method
-        
+        // Q1> Create a function which calculates the sum and product of elements of Array
+        // Q2> Find the complexity for created method
+
+        // Answer:-
+        interviewQuestion1 arr = new interviewQuestion1();
+        int [] customArray = {1,3,5,7};
+        arr.spofArray(customArray);
+
+    }
+
+    void spofArray(int [] array) {
+        int sum = 0;
+        int product = 1;
+        for (int i=0;i<array.length;i++) {
+            sum += array[i];
+        }
+        for (int i=0;i<array.length;i++) {
+            product *= array[i];
+        }
+        System.out.println(sum+" , "+product);
     }
 }
