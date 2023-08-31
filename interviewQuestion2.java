d81218622caf8a1f7873da8ff52d977b8952d7b1
pralayesh <rajmukherjee2807@gmail.com>
2023-08-31 23:42:22 +0530

    write a new function to solve the interview first question where I'm using nested loop for creating the pairs and in the main function I'm creating an object to access the new function and also create an array and call this

--- interviewQuestion2.java
+++ interviewQuestion2.java
@@ -2,5 +2,16 @@ public class interviewQuestion2{
     public static void main(String[] args) {
         // Create a function which prints to the console the pairs from given array.
         // Finds the time complexity for the created method.
+
+        interviewQuestion2 arr = new interviewQuestion2();
+        int [] customArray = {1,3,5,7};
+        arr.printPairs(customArray);
     }
-}
+    void printPairs(int [] array) {
+        for(int i=0;i<array.length;i++) {
+            for(int j=0;j<array.length;j++) {
+                System.out.println(array[i] + " , " + array[j]);
+            }
+        }
+    }
+} 
