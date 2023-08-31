6f96f1476d3ff831a4d4a0097b1f1a94008332b8
pralayesh <rajmukherjee2807@gmail.com>
2023-08-31 23:34:08 +0530

    write the Interview Questions

---
+++ interviewQuestion2.java
@@ -0,0 +1,6 @@
+public class interviewQuestion2{
+    public static void main(String[] args) {
+        // Create a function which prints to the console the pairs from given array.
+        // Finds the time complexity for the created method.
+    }
+}
