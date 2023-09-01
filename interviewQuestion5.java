e0b23dff573e88bf08f30ec7b6478835281e0b35
pralayesh <rajmukherjee2807@gmail.com>
2023-09-01 13:47:45 +0530

    add the given method to explain the runtime complexity

--- interviewQuestion5.java
+++ interviewQuestion5.java
@@ -1,5 +1,19 @@
 public class interviewQuestion5{
     public static void main(String[] args) {
         // What is the runtime complexity of the below code?
+
+        interviewQuestion5 arr = new interviewQuestion5();
+        int[] customArray1 = {1,3,5,7};
+        int[] customArray2 = {2,4,6,8};
+        arr.printPairs(customArray1, customArray2);
+    }
+    void printPairs(int[] arrayA, int[] arrayB) {
+        for(int i=0;i<arrayA.length;i++) {
+            for(int j=0;j<arrayB.length;j++) {
+                for(int k=0;k<1000000;k++) {
+                    System.out.println(arrayA[i]+" , "+arrayB[j]);
+                }
+            }
+        }
     }
 }
