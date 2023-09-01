eb762b887f6c8e7f2c060e595a38543a59fde5d3
pralayesh <rajmukherjee2807@gmail.com>
2023-09-01 11:59:09 +0530

    add the method for the question when pairs are decreasea by 1

--- interviewQuestion3.java
+++ interviewQuestion3.java
@@ -1,5 +1,16 @@
 public class interviewQuestion3{
     public static void main(String[] args) {
         // What is the runtime complexity for the given method
+        interviewQuestion3 arr = new interviewQuestion3();
+        int [] customArray = {1,3,5,7};
+        arr.printPairs(customArray);
+    }
+
+    void printPairs(int [] array) {
+        for(int i=0;i<array.length;i++) {
+            for(int j=i+1;j<array.length;j++) {
+                System.out.println(array[i]+" , "+array[j]);
+            }
+        }
     }
 }
