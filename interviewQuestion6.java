18010f948fa00a8f4fff13b3cfe4ee7136cad371
pralayesh <rajmukherjee2807@gmail.com>
2023-09-02 10:52:57 +0530

    access the reverseArray function by using arr object

--- interviewQuestion6.java
+++ interviewQuestion6.java
@@ -4,6 +4,7 @@ public class interviewQuestion6{
         // Find the runtime complexity of the created method
         interviewQuestion6 arr = new interviewQuestion6();
         int[] customArray = {1,2,3,4,5};
+        arr.rseserveArray(customArray);
     }
     void rseserveArray(int[] array) {
         for(int i=array.length-1;i<=0;i--) {
