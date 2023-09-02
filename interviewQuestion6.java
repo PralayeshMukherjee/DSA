8da0f362d5a2685285a8acaf4c6a38ed867fa524
pralayesh <rajmukherjee2807@gmail.com>
2023-09-02 10:24:54 +0530

    add the new object in the main function to access the method

--- interviewQuestion6.java
+++ interviewQuestion6.java
@@ -2,7 +2,7 @@ public class interviewQuestion6{
     public static void main(String[] args) {
         // Created a method which takes an array as a parameter and reverse it
         // Find the runtime complexity of the created method
-        
+        interviewQuestion6 arr = new interviewQuestion6();
     }
     void rseserveArray(int[] array) {
         for(int i=array.length-1;i<=0;i--) {
