8bd5c73909510b83203b6f53ebfdbf5ed062f0e9
pralayesh <rajmukherjee2807@gmail.com>
2023-09-02 10:23:37 +0530

    write the function or method to reverse the input array and taking array as a parameter

--- interviewQuestion6.java
+++ interviewQuestion6.java
@@ -2,5 +2,11 @@ public class interviewQuestion6{
     public static void main(String[] args) {
         // Created a method which takes an array as a parameter and reverse it
         // Find the runtime complexity of the created method
+        
+    }
+    void rseserveArray(int[] array) {
+        for(int i=array.length-1;i<=0;i--) {
+            System.out.println(array);
+        }
     }
 }
