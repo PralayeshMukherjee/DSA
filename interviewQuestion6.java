46209a9b000387f938e3454e912bd268bbdcfba4
pralayesh <rajmukherjee2807@gmail.com>
2023-09-02 10:27:02 +0530

    add the array which I want to reverse and that is integer type

--- interviewQuestion6.java
+++ interviewQuestion6.java
@@ -3,6 +3,7 @@ public class interviewQuestion6{
         // Created a method which takes an array as a parameter and reverse it
         // Find the runtime complexity of the created method
         interviewQuestion6 arr = new interviewQuestion6();
+        int[] customArray = {1,2,3,4,5};
     }
     void rseserveArray(int[] array) {
         for(int i=array.length-1;i<=0;i--) {
