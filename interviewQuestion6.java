27164707dddc45ebc5b21c4a1aafc12a1bb55b04
pralayesh <rajmukherjee2807@gmail.com>
2023-09-03 20:33:42 +0530

    add another temporary variable to store first part array elements

--- interviewQuestion6.java
+++ interviewQuestion6.java
@@ -11,6 +11,7 @@ public class interviewQuestion6{
     void rseserveArray(int[] array) {
         for(int i=0;i<array.length/2;i++){
             int other = array.length-i-1;
+            int temp = array[i];
         }
     }
 }
