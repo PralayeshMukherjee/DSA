1857773ca17a0271b7333af460c3018e13897e3f
pralayesh <rajmukherjee2807@gmail.com>
2023-09-03 20:34:43 +0530

    change the positions of the array from first to last

--- interviewQuestion6.java
+++ interviewQuestion6.java
@@ -12,6 +12,7 @@ public class interviewQuestion6{
         for(int i=0;i<array.length/2;i++){
             int other = array.length-i-1;
             int temp = array[i];
+            array[i] = array[other];
         }
     }
 }
