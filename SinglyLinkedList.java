8eea5ad4c3e8556451caafb6b9746998cb76b787
pralayesh <rajmukherjee2807@gmail.com>
2023-09-04 23:02:23 +0530

    create the head

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -2,4 +2,15 @@ public class SinglyLinkedList{
     public Node head;
     public Node tail;
     public int size;
+
+    public Node createSinglyLinkedList(int nodeValue) {
+        head = new Node();
+        Node node = new Node();
+        node.next = null;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    } 
 }
