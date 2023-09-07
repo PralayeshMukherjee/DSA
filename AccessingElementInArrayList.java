c4897d15578f90a6845c99247822ae1581533eaf
pralayesh <rajmukherjee2807@gmail.com>
2023-09-07 21:31:26 +0530

    explain properly

--- AccessingElementInArrayList.java
+++ AccessingElementInArrayList.java
@@ -4,12 +4,14 @@ public class AccessingElementInArrayList{
     public static void main(String[] args) {
         // create an ArrayList
         ArrayList<String> names = new ArrayList<String>();
+        
         // insert all names
         names.add("raj");
         names.add("sarbojit");
         names.add("maharnab");
         names.add(2,"anjan");
         System.out.println(names);
+
         // Accessing Elements in ArrayList
         String nameAt3rdIndex = names.get(2);
         String nameAt4thIndex = names.get(3);
