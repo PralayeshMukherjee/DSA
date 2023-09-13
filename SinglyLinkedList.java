37c5ab8235f2b87e1c23d3eb7d555f5d28aa40ab
pralayesh <rajmukherjee2807@gmail.com>
2023-09-13 18:53:13 +0530

    if the head is equal to null then I just call the createSinglyLinkedList function

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -20,5 +20,8 @@ public class SinglyLinkedList{
     public void insertInLinkedList(int nodeValue, int location) {
         Node node = new Node();
         node.value = nodeValue;
+        if(head == null) {
+            createSinglyLinkedList(nodeValue);
+        }
     }
 }
