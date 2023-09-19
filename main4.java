8f1c9b6c6c470a7a87c6f8fd598073ea43fef12d
pralayesh <rajmukherjee2807@gmail.com>
2023-09-19 18:57:05 +0530

    create the singly linked list function

--- main4.java
+++ main4.java
@@ -1,9 +1,10 @@
 public class main4{
     public static void main(String[] args) {
+        // Create a Singly Linked List
         SinglyLinkedList sLL = new SinglyLinkedList();
         sLL.createSinglyLinkedList(5);
         System.out.println(sLL.head.value);
-
+        
         sLL.insertInLinkedList(6, 0);
         System.out.println(sLL.head.value);
 
@@ -12,5 +13,7 @@ public class main4{
         sLL.insertInLinkedList(8, 4);
         sLL.insertInLinkedList(9, 5);
         sLL.traverseSinglyLinkedList();
+
+
     }
 }
